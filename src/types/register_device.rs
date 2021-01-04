df2f26d63fe52f04e00034e948b20282c1ec3d88
Thomas Versteeg <thomas@versteeg.email>
2021-01-04 20:56:17 +0100

    Add verification object and register response

--- src/types/register_device.rs
+++ src/types/register_device.rs
@@ -44,6 +44,8 @@ pub struct RegisterDeviceResponse {
     name: String,
     /// The public key of the server.
     server_public_key: String,
+    /// Verification code that needs to match on all devices.
+    verification: String,
 }
 
 impl RegisterDeviceResponse {
@@ -51,15 +53,22 @@ impl RegisterDeviceResponse {
     ///
     /// Used by the server applications.
     #[doc(hidden)]
-    pub fn new<S1, S2>(id: S1, name: S2, server_public_key: &PublicKey) -> Self
+    pub fn new<S1, S2, S3>(
+        id: S1,
+        name: S2,
+        server_public_key: &PublicKey,
+        verification: S3,
+    ) -> Self
     where
         S1: Into<String>,
         S2: Into<String>,
+        S3: Into<String>,
     {
         Self {
             id: id.into(),
             name: name.into(),
             server_public_key: base64::encode(server_public_key.as_bytes()),
+            verification: verification.into(),
         }
     }
 
@@ -73,7 +82,7 @@ impl RegisterDeviceResponse {
         &self.name
     }
 
-    /// Get the public key of the server.
+    /// Public key of the server.
     pub fn server_public_key(&self) -> Result<PublicKey> {
         // Read exactly the bytes from the public key
         let bytes: [u8; 32] = base64::decode(self.server_public_key.clone())
@@ -82,5 +91,57 @@ impl RegisterDeviceResponse {
             .map_err(|_| anyhow!("Device public key is invalid"))?;
 
         Ok(PublicKey::from(bytes))
+    }
+
+    /// Unique verification code that needs to match on all devices before the device can be
+    /// registered.
+    pub fn verification_code(&self) -> &str {
+        &self.verification
+    }
+}
+
+/// A device that still needs to be verified.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct NeedsVerificationDevice {
+    /// Unique identifier.
+    id: String,
+    /// Name of the device as configured by the user.
+    name: String,
+    /// Verification code that needs to match on all devices.
+    verification: String,
+}
+
+impl NeedsVerificationDevice {
+    /// Create a new response.
+    ///
+    /// Used by the server applications.
+    #[doc(hidden)]
+    pub fn new<S1, S2, S3>(id: S1, name: S2, verification: S3) -> Self
+    where
+        S1: Into<String>,
+        S2: Into<String>,
+        S3: Into<String>,
+    {
+        Self {
+            id: id.into(),
+            name: name.into(),
+            verification: verification.into(),
+        }
+    }
+
+    /// Unique identifier.
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+
+    /// Name of the device as configured by the user.
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Unique verification code that needs to match on all devices before the device can be
+    /// registered.
+    pub fn verification_code(&self) -> &str {
+        &self.verification
     }
 }

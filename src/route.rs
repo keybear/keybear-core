1bb939262af8bd4ce21f4654bea816ab4997a634
Thomas Versteeg <thomas@versteeg.email>
2021-01-09 10:34:18 +0100

    Use full route names

--- src/route.rs
+++ src/route.rs
@@ -1,13 +1,13 @@
 /// Define the routes by API version.
 pub mod v1 {
     /// Registration of new devices.
-    pub const REGISTER: &str = "/register";
+    pub const REGISTER: &str = "/v1/register";
     /// Verification of newly registered devices.
-    pub const VERIFY: &str = "/verify";
+    pub const VERIFY: &str = "/v1/verify";
     /// All the devices that need to be verified.
-    pub const VERIFICATION_DEVICES: &str = "/verification_devices";
+    pub const VERIFICATION_DEVICES: &str = "/v1/verification_devices";
     /// All the devices which have gone through the full verification process.
-    pub const DEVICES: &str = "/devices";
+    pub const DEVICES: &str = "/v1/devices";
     /// All passwords.
-    pub const PASSWORD: &str = "/passwords";
+    pub const PASSWORD: &str = "/v1/passwords";
 }
